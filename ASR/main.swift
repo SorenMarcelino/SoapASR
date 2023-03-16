90de2afa11bddd5f6f9acb47244342f92f5613b7
Soren Marcelino <51518813+SorenMarcelino@users.noreply.github.com>
2023-03-16 10:36:29 +0100

    ASR as SOAP Web Service

--- ASR/main.swift
+++ ASR/main.swift
@@ -5,16 +5,43 @@
 //  Created by Soren Marcelino on 07/03/2023.
 //
 
+//MARK: ASR fonctionne en SOAP
 import Foundation
 import Speech
 import Swifter
+import Socket
 
 let server = HttpServer()
 
+class XMLParserDelegateCustom: NSObject, XMLParserDelegate {
+    var foundAudioData = false
+        var audioData: Data?
+        
+        func parser(_ parser: XMLParser, foundCharacters string: String) {
+            if foundAudioData {
+                audioData = Data(base64Encoded: string, options: .ignoreUnknownCharacters)
+                foundAudioData = false
+                print("Audio data decoded successfully.")
+            }
+        }
+        
+        func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+            if elementName == "audio" {
+                foundAudioData = true
+            }
+        }
+}
+
 func transcribeAudio(request: HttpRequest) -> HttpResponse {
-    guard let audioData = try? Data(contentsOf: URL(fileURLWithPath: "/Users/soren/Desktop/ASR/ASR/MacronASRTestPart.mp3")),
-          let audioURL = writeAudioDataToFile(data: audioData) else {
-        return .notFound
+    let data = Data(request.body)
+    let parser = XMLParser(data: data)
+    let delegate = XMLParserDelegateCustom()
+    parser.delegate = delegate
+    parser.parse()
+        
+    // Decode the base64-encoded audio data
+    guard let audioData = delegate.audioData, let audioURL = writeAudioDataToFile(data: audioData) else {
+        return .internalServerError
     }
 
     let recognizer = SFSpeechRecognizer(locale: Locale(identifier: "fr-FR"))
@@ -37,17 +64,6 @@ func transcribeAudio(request: HttpRequest) -> HttpResponse {
             print("Transcription Finale: \(transcription ?? "")")
             semaphore.signal()
         }
-
-        /*guard error == nil else {
-            print("Error: \(error!)")
-            semaphore.signal()
-            return
-        }*/
-
-        /*transcription = result?.bestTranscription.formattedString
-        print("Transcription: \(transcription ?? "")")
-        //semaphore.signal()*/
-        
     }
     
     _ = semaphore.wait(timeout: .distantFuture)
@@ -73,8 +89,28 @@ func transcribeAudio(request: HttpRequest) -> HttpResponse {
     })
 }
 
+func helloWorld(request: HttpRequest) -> HttpResponse {
+    let soapResponse = """
+    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+        <soap:Body>
+            <transcription>Bonjour le Monde</transcription>
+        </soap:Body>
+    </soap:Envelope>
+    """
+    return HttpResponse.raw(200, "OK", ["Content-Type": "application/xml"], { writer in
+        guard let data = soapResponse.data(using: .utf8) else {
+            print("ERROR 500")
+            return
+        }
+        try writer.write(data)
+    })}
+
 server.POST["/transcribe"] = { request in
     return transcribeAudio(request: request)
+}
+
+server.GET["/"] = { request in
+    return helloWorld(request: request)
 }
 
 func writeAudioDataToFile(data: Data) -> URL? {
@@ -91,8 +127,9 @@ func writeAudioDataToFile(data: Data) -> URL? {
 }
 
 do {
-    try server.start(45876)
-    print("Server is running on http://localhost:45876/")
+    try server.start(45876, forceIPv4: true)
+    
+    print("Server is running on http://192.168.1.154:45876/")
     RunLoop.main.run()
 } catch {
     print("Error starting server: \(error)")
